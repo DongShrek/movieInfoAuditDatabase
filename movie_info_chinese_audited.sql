1f665e0c7180da137a104d1d94d8c7da10755ee9
DongShrek <46716455+DongShrek@users.noreply.github.com>
2022-05-24 20:10:01 +0800

    Update and rename main.sql to movie_info_chinese_audited.sql

--- movie_info_chinese_audited.sql
+++ movie_info_chinese_audited.sql
@@ -1,17 +1,18 @@
-# 1. 创建电影中文名表
-create table movie_chinese_title_audited
+# 创建电影中文表
+create table movie_info_chinese_audited
 select m.ImdbId, m.chineseTitle
 from imdb_douban_movie m;
-alter table movie_chinese_title_audited
+
+alter table movie_info_chinese_audited
     add primary key (ImdbId(255));
 
-## 电影中文名有增加则表更新
-replace into movie_chinese_title_audited (ImdbId, chineseTitle)
+## 电影中文有增加则表更新
+replace into movie_info_chinese_audited (ImdbId, chineseTitle)
 select m.ImdbId, m.chineseTitle
 from imdb_douban_movie m;
 
 ## 利用TMDB更新缺失的英文名
-replace into movie_chinese_title_audited (ImdbId, chineseTitle)
+replace into movie_info_chinese_audited (ImdbId, chineseTitle)
 select m.ImdbId, t.movieTitle
 from imdb_douban_movie m,
      tmdb_movie_chinese t

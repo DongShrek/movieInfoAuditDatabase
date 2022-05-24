0200fe133541622e9cf359d24d893f9fd88ec508
DongShrek <46716455+DongShrek@users.noreply.github.com>
2022-05-25 05:07:37 +0800

    Update movie_info_chinese_audited.sql

--- movie_info_chinese_audited.sql
+++ movie_info_chinese_audited.sql
@@ -7,12 +7,12 @@ alter table movie_info_chinese_audited
     add primary key (ImdbId(255));
 
 ## 电影中文有增加则表更新
-replace into movie_info_chinese_audited (ImdbId, chineseTitle)
+insert ignore into movie_info_chinese_audited (ImdbId, chineseTitle)
 select m.ImdbId, m.chineseTitle
 from imdb_douban_movie m;
 
 ## 利用TMDB更新缺失的英文名
-replace into movie_info_chinese_audited (ImdbId, chineseTitle)
+insert ignore into movie_info_chinese_audited (ImdbId, chineseTitle)
 select m.ImdbId, t.movieTitle
 from imdb_douban_movie m,
      tmdb_movie_chinese t

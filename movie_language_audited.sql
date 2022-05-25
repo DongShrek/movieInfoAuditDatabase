d7030b08bf2ba3c145b4a77093b93a7b308a04ed
DongShrek <46716455+DongShrek@users.noreply.github.com>
2022-05-25 21:13:09 +0800

    Create movie_language_audited.sql

---
+++ movie_language_audited.sql
@@ -0,0 +1,8 @@
+create table movie_language_audited (select tm.ImdbId, la.chineseName, tm.originalLanguage
+                                     from tmdb_movie_chinese as tm
+                                              left join (select *
+                                                         from (select originalLanguage
+                                                               from tmdb_movie_chinese
+                                                               group by originalLanguage) as lu
+                                                                  left join language_conversion on lu.originalLanguage = languageCode) as la
+                                                        on la.languageCode = tm.originalLanguage);

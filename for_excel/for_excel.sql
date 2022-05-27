006e68a4c39b71dadad1b2d41773b26051b0e0cb
pomme <dongshrek@gmail.com>
2022-05-28 05:14:38 +0800

    上榜时间表增加movies_analysis_list限制

--- for_excel/for_excel.sql
+++ for_excel/for_excel.sql
@@ -9,25 +9,29 @@ select * from (select originalLanguage from tmdb_movie_chinese group by original
 left join language_conversion on languageCodeTable.originalLanguage=language_conversion.languageCode;
 
 # 在榜时间表 9X3KC8SR
-select ImdbId,
-       movieTitle,
-       startDay,
-       startDayNumber,
-       endDay,
-       endDayNumber,
-       dayCount,
-       (endDayNumber - startDayNumber + 1)            duration,
-       (endDayNumber - startDayNumber + 1 - dayCount) difference
-from (select startEndDayTable.ImdbId   ImdbId,
-             startEndDayTable.Title    movieTitle,
-             startEndDayTable.startDay startDay,
-             daylist1.serialNumber     startDayNumber,
-             startEndDayTable.endDay   endDay,
-             daylist2.serialNumber     endDayNumber,
-             startEndDayTable.dayCount
-      from (select ImdbId, Title, min(day) startDay, max(day) endDay, count(day) dayCount
-            from imdb_history where day<'2022-01-01'
-            group by ImdbId, Title) as startEndDayTable
-               left join imdb_250_day_list as daylist1 on startday = daylist1.day
-               left join imdb_250_day_list as daylist2 on endday = daylist2.day) as movieDuration
-order by movieDuration.endDay desc, movieDuration.startDay;
+select *
+from (select ImdbId,
+             movieTitle,
+             startDay,
+             startDayNumber,
+             endDay,
+             endDayNumber,
+             dayCount,
+             (endDayNumber - startDayNumber + 1)            duration,
+             (endDayNumber - startDayNumber + 1 - dayCount) difference
+      from (select startEndDayTable.ImdbId   ImdbId,
+                   startEndDayTable.Title    movieTitle,
+                   startEndDayTable.startDay startDay,
+                   daylist1.serialNumber     startDayNumber,
+                   startEndDayTable.endDay   endDay,
+                   daylist2.serialNumber     endDayNumber,
+                   startEndDayTable.dayCount
+            from (select ImdbId, Title, min(day) startDay, max(day) endDay, count(day) dayCount
+                  from imdb_history
+                  where day < '2022-01-01'
+                  group by ImdbId, Title) as startEndDayTable
+                     left join imdb_250_day_list as daylist1 on startday = daylist1.day
+                     left join imdb_250_day_list as daylist2 on endday = daylist2.day) as movieDuration) a,
+     movies_analysis_list
+where a.ImdbId = movies_analysis_list.ImdbId
+order by a.endDay desc, a.startDay;

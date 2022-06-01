522250a056599e8dcf38081d2ffddb50cac50c72
pomme <dongshrek@gmail.com>
2022-06-02 01:41:17 +0800

    电影语言表更新

--- movie_language_audited.sql
+++ movie_language_audited.sql
@@ -1,3 +1,4 @@
+# 创建初始表
 create table movie_language_audited (select tm.ImdbId, la.languageInChinese, tm.originalLanguage
                                      from tmdb_movie_chinese as tm
                                               left join (select *
@@ -6,3 +7,20 @@ create table movie_language_audited (select tm.ImdbId, la.languageInChinese, tm.
                                                                group by originalLanguage) as lu
                                                                   left join language_conversion on lu.originalLanguage = languageCode) as la
                                                         on la.languageCode = tm.originalLanguage);
+
+alter table movie_language_audited
+    add primary key (ImdbId(255));
+
+# 更新策略
+insert ignore into movie_language_audited(ImdbId, languageInChinese, originalLanguage)
+select ImdbId, languageInChinese, originalLanguage
+from (select tm.ImdbId, la.languageInChinese, tm.originalLanguage
+      from tmdb_movie_chinese as tm
+               left join (select *
+                          from (select originalLanguage
+                                from tmdb_movie_chinese
+                                group by originalLanguage) as lu
+                                   left join language_conversion on lu.originalLanguage = languageCode) as la
+                         on la.languageCode = tm.originalLanguage) as languageList
+
+where ImdbId not in (select ImdbId from movie_language_audited)

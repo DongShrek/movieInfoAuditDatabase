f708e80ef147fa2be1cc52790a45a939c755b69c
pomme <dongshrek@gmail.com>
2022-06-02 01:47:26 +0800

    电影语言更新

--- movie_language_audited.sql
+++ movie_language_audited.sql
@@ -13,14 +13,12 @@ alter table movie_language_audited
 
 # 更新策略
 insert ignore into movie_language_audited(ImdbId, languageInChinese, originalLanguage)
-select ImdbId, languageInChinese, originalLanguage
-from (select tm.ImdbId, la.languageInChinese, tm.originalLanguage
+select tm.ImdbId, la.languageInChinese, tm.originalLanguage
       from tmdb_movie_chinese as tm
                left join (select *
                           from (select originalLanguage
                                 from tmdb_movie_chinese
                                 group by originalLanguage) as lu
                                    left join language_conversion on lu.originalLanguage = languageCode) as la
-                         on la.languageCode = tm.originalLanguage) as languageList
-
+                         on la.languageCode = tm.originalLanguage
 where ImdbId not in (select ImdbId from movie_language_audited)

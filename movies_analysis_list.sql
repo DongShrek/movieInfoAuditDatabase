aae7e3e0f07c241619dbb77b418386fdd33daaf5
pomme <dongshrek@gmail.com>
2022-06-02 01:53:41 +0800

    需要分析的电影列表

--- movies_analysis_list.sql
+++ movies_analysis_list.sql
@@ -4,3 +4,12 @@ create table movies_analysis_list select ImdbId,Title from movie_basic_info
 # 是否属于需要分析的电影
 alter table movies_analysis_list
     add isNeedAnalysis text null;
+
+
+alter table movies_analysis_list
+    add primary key (ImdbId(255));
+
+# 更新策略
+insert ignore into movies_analysis_list (ImdbId, Title)
+select ImdbId, Title
+from movie_basic_info where ImdbId not in (select ImdbId from movies_analysis_list)

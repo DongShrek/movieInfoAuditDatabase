2d5b7db70a32f7fa84018c701cfc328e91dd36d8
pomme <dongshrek@gmail.com>
2022-06-04 05:35:08 +0800

    tmdb更新导演表

--- movies_director_audited.sql
+++ movies_director_audited.sql
@@ -2,3 +2,10 @@
 create table movies_director_audited
 select ImdbId, chineseTitle, ImdbTitle, director
 from imdb_douban_movie;
+
+# 用tmdb名字更新电影名
+update movies_director_audited a
+set a.chineseTitle=(select t.movieTitle
+                    from tmdb_movie_chinese t
+                    where t.ImdbId = a.ImdbId)
+where a.chineseTitle is null;

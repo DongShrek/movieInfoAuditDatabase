840130376e3417fa75b397e9ead200738dfc4bdf
pomme <dongshrek@gmail.com>
2022-06-05 00:18:13 +0800

    新语言参照表

---
+++ language_conversion.sql
@@ -0,0 +1,38 @@
+# 创建语言代码对照表
+create table mydb.language_conversion
+(
+    languageCode text null,
+    languageInChinese  text null
+);
+
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('de','德语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('en','英语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ru','俄语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('fr','法语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('it','意大利语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('es','西班牙语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ja','日语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('bn','孟加拉语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('sv','瑞典语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ro','罗马尼亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('hi','印度语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('cn','中文');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('sh','塞尔维亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('zh','中文');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('da','丹麦语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('mk','马其顿语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('nl','荷兰语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('sr','塞尔维亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('tr','土耳其语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('cs','捷克语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('fa','波斯语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('pt','葡萄牙语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('bs','波斯尼亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ko','朝鲜语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ta','印度语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('he','希伯来语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ml','印度语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('id','印度尼西亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('te','印度语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('et','爱沙尼亚语');
+INSERT INTO mydb.language_conversion (languageCode, languageInChinese) VALUES ('ar','阿拉伯语');

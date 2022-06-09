65ace9f69b1d66b5669c5035365c326be20055d1
pomme <dongshrek@gmail.com>
2022-06-09 23:08:46 +0800

    电影导演表更新策略

--- movies_director_audited.sql
+++ movies_director_audited.sql
@@ -3,6 +3,14 @@ create table movies_director_audited
 select ImdbId, chineseTitle, ImdbTitle, director
 from imdb_douban_movie;
 
+alter table movies_director_audited
+    add primary key (ImdbId(255));
+
+## 未来更新策略：电影有增加则表更新
+insert ignore into movies_director_audited (ImdbId, chineseTitle, ImdbTitle, director)
+select ImdbId, chineseTitle, ImdbTitle, director
+from imdb_douban_movie;
+
 # 用tmdb名字更新电影名
 update movies_director_audited a
 set a.chineseTitle=(select t.movieTitle

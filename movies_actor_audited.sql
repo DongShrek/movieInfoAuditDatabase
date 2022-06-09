ee5e37a2067812865f2b00bade0822505251312e
pomme <dongshrek@gmail.com>
2022-06-10 01:25:55 +0800

    手动修改演员表的部分

--- movies_actor_audited.sql
+++ movies_actor_audited.sql
@@ -3,6 +3,14 @@ create table movies_actor_audited
 select ImdbId, chineseTitle, ImdbTitle,actor
 from imdb_douban_movie;
 
+alter table movies_actor_audited
+    add primary key (ImdbId(255));
+
+## 未来更新策略：电影有增加则表更新
+insert ignore into movies_actor_audited (ImdbId, chineseTitle, ImdbTitle,actor)
+select ImdbId, chineseTitle, ImdbTitle, actor
+from imdb_douban_movie;
+
 # 用tmdb名字更新电影名
 update movies_actor_audited a
 set a.chineseTitle=(select t.movieTitle

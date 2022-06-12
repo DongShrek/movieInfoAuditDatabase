1efafb4d60e911691af85cf058d0fc7c6d0c48ac
pomme <dongshrek@gmail.com>
2022-06-13 04:32:56 +0800

    电影出版名称日期改动，为避免歧义

--- movies_publish_date_audited.sql
+++ movies_publish_date_audited.sql
@@ -1,13 +1,13 @@
 # 创建电影日期表
 create table movies_publish_date_audited
-select m.ImdbId,m.Title ImdbTitle,i.chineseTitle,m.Year publish_year,i.year publish_year_douban,i.pubdates publish_date from movie_basic_info m
+select m.ImdbId,m.Title ImdbTitle,i.chineseTitle,m.Year publish_year_imdb,i.year publish_year_douban,i.pubdates publish_date from movie_basic_info m
 left join imdb_douban_movie i on m.ImdbId=i.ImdbId;
 
 alter table movies_publish_date_audited
     add primary key (ImdbId(255));
 
 ## 未来更新策略：电影有增加则表更新
-insert ignore into movies_publish_date_audited (ImdbId,ImdbTitle,chineseTitle,publish_year,publish_year_douban,publish_date)
+insert ignore into movies_publish_date_audited (ImdbId,ImdbTitle,chineseTitle,publish_year_imdb,publish_year_douban,publish_date)
 select m.ImdbId,m.Title,i.chineseTitle,m.Year,i.year,i.pubdates from movie_basic_info m
 left join imdb_douban_movie i on m.ImdbId=i.ImdbId;
 
